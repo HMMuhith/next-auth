f05238f227c86b28e7e79009650a454e7a9d6d06
Muhith <muhithkhan8@gmail.com>
2025-11-04 11:28:50 +0600

    border changed

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -6,7 +6,7 @@ return (<>
     <div className="text-5xl text-white pl-20 pt-6">
         Logo
     </div>
-    <ul className="flex justify-center w-[40rem] gap-13 text-white pt-6 items-center border border-red-700">
+    <ul className="flex justify-center w-[40rem] gap-13 text-white pt-6 items-center ">
       
         <li><a className="text-sm hover:underline-offset-8 hover:underline" href="/login">Log in</a></li>
         <li><a className="text-sm hover:underline-offset-8 hover:underline" href="/signup">Sign up</a></li>
