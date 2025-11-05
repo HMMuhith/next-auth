b4eed4bc0be28408fa267047219ecf954d87bee7
Muhith <muhithkhan8@gmail.com>
2025-11-05 17:57:25 +0600

    front page changed

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+'use client'
 
+import { useEffect, useState } from "react";
 
 export default function Main() {
+  const [user,setuser]=useState('')
+  useEffect(()=>{
+    const request=localStorage.getItem('user')
+    setuser(request)
+  },[])
   return (<>
       
-   <div className="m-auto text-center bg-slate-900 flex justify-center items-center w-dvw text-2xl  text-green-600 min-h-screen  ">Hi Muhith</div>
+   <div className="m-auto text-center bg-slate-900 flex justify-center pt-36 w-dvw text-4xl  text-sky-400 min-h-screen  ">Hi { user || `Guest`} </div>
    </>
   );
 }
